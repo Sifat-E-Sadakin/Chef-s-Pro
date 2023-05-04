562ae86d50730b5951a2f1ed95188fae0ce9250c
Sifat-E-Sadakin <sifatesadakin10@gmail.comm>
2023-05-04 16:43:53 +0600

    react tooltip implemented

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,10 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { userContext } from '../UserProvider/UserProvider';
 import ActiveLink from '../ActiveLink/ActiveLink';
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from 'react-tooltip'
+import { Tooltip as ReactTooltip } from 'react-tooltip'
+
 
 const Navbar = () => {
 
@@ -18,15 +22,15 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <ActiveLink to='/'>Home</ActiveLink>
-                        <ActiveLink to='/recipes'>Chef Details</ActiveLink>
-                        <ActiveLink to='/favRecipes'>Favorite Recipes</ActiveLink>
+                            <ActiveLink to='/'>Home</ActiveLink>
+                            <ActiveLink to='/recipes'>Chef Details</ActiveLink>
+                            <ActiveLink to='/favRecipes'>Favorite Recipes</ActiveLink>
 
-                        {user? <></>:<ActiveLink to='/login'>Login</ActiveLink>}
-                        {user?<></>:<ActiveLink to='/signUp'>SignUp</ActiveLink>}
-                        <ActiveLink to='/blogs'>Blogs</ActiveLink>
+                            {user ? <></> : <ActiveLink to='/login'>Login</ActiveLink>}
+                            {user ? <></> : <ActiveLink to='/signUp'>SignUp</ActiveLink>}
+                            <ActiveLink to='/blogs'>Blogs</ActiveLink>
 
-                        {user && <button onClick={logout} >Sing Out</button>}
+                            {user && <button onClick={logout} >Sing Out</button>}
                         </ul>
                     </div>
                     <a className="btn btn-ghost normal-case text-xl">Chef's Pro</a>
@@ -37,8 +41,8 @@ const Navbar = () => {
                         <ActiveLink to='/recipes'>Chef Details</ActiveLink>
                         <ActiveLink to='/favRecipes'>Favorite Recipes</ActiveLink>
 
-                        {user? <></>:<ActiveLink to='/login'>Login</ActiveLink>}
-                        {user?<></>:<ActiveLink to='/signUp'>SignUp</ActiveLink>}
+                        {user ? <></> : <ActiveLink to='/login'>Login</ActiveLink>}
+                        {user ? <></> : <ActiveLink to='/signUp'>SignUp</ActiveLink>}
                         <ActiveLink to='/blogs'>Blogs</ActiveLink>
 
                         {user && <button onClick={logout} >Sing Out</button>}
@@ -46,8 +50,8 @@ const Navbar = () => {
                 </div>
                 <div className="navbar-end">
                     {user && <h1 className='mr-3'>Welcome <span className='font-bold'>{user.displayName}</span> </h1>}
-                    {user ?<><ActiveLink  to='/userinfo'><img  title={user.displayName} src={user.photoURL} className='rounded-full w-12'></img></ActiveLink></>  : <Link className='btn btn-primary' to='/login'>Login</Link>}
-
+                    {user ? <><ActiveLink  to='/userinfo'><img data-tooltip-id="my-tooltip" data-tooltip-content={user.displayName} title={user.displayName} src={user.photoURL} className='rounded-full w-12'></img></ActiveLink></> : <Link className='btn btn-primary' to='/login'>Login</Link>}
+                    <Tooltip id="my-tooltip" />
                 </div>
             </div>
         </div>
